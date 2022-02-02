bc4e35acb702c1ff53029ec1ca8cc89e9fb07a34
danielbarra24 <72272441+danielbarra24@users.noreply.github.com>
2022-02-02 02:54:19 -0500

    backend and default typescript set up

---
+++ src/server/config/createServer.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import 'dotenv/config';
+
+const createServer = (): express.Express => {
+  const app = express();
+  // sets the port for the app
+  app.set('port', process.env.PORT || 5000);
+  // gives express the ability to parse requessts with JSON and turn the JSON into objects
+  app.use(express.json());
+  // gives express the ability to parse urlencoded payloads
+  app.use(
+    express.urlencoded({
+      extended: true,
+    }),
+  );  
+  // gives express the ability accept origins outside its own to accept requests from
+  app.use(cors());
+
+
+  // Serving static files
+  if (process.env.NODE_ENV === 'production') {
+    const root = path.join(__dirname, '..', 'client', 'build');
+
+    app.use(express.static(root));
+    app.get('*', (_: any, res: any) => {
+      res.sendFile('index.html', { root });
+    });
+  }
+
+  return app;
+};
+
+export default createServer;
