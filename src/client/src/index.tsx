7b9907432d8bab78d981c73155bc234bd9b960bb
danielbarra24 <72272441+danielbarra24@users.noreply.github.com>
2022-02-27 14:43:27 -0500

    updated index page

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HomeView from './home/HomeView';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomeView />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
