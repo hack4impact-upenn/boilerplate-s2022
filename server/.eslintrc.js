60bc4914a75e9d984ef24f8f8585dd8cdfe91827
taran317 <67604374+taran317@users.noreply.github.com>
2022-03-16 20:57:19 -0400

    fixed eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
   rules: {
-<<<<<<< HEAD:server/.eslintrc.js
     'no-console': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-=======
     // Add overriden rules specific to server here
     'no-underscore-dangle': [
       'error',
       { allowAfterThis: true, allow: ['_instance'] },
     ],
->>>>>>> master:src/server/.eslintrc.js
   },
 };
