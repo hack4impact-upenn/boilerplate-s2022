a0b5d7012aafa99eca5b08b0fa561389ac277539
Katherine Wang <katherinewang@huntsman-ve703-13991.apn.wlan.private.upenn.edu>
2022-03-28 19:42:37 -0400

    styled components

--- src/client/src/pages/NotFoundPage.tsx
+++ src/client/src/pages/NotFoundPage.tsx
@@ -1,35 +1,41 @@
+import styled from 'styled-components';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+// import { createStore } from 'redux';
+// import reducers from '../redux/store/reducers';
 
-const styles = {
-  container: {
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontFamily: 'Helvetica',
-    margin: 'auto',
-  },
-  box: {
-    backgroundColor: '#ededed',
-    width: '60%',
-    height: '60%',
-    padding: '30px 100px',
-    borderRadius: '10px',
-    boxShadow: '1px 1px 10px rgba(0,0,0,0.2)',
-  },
-  header: {
-    color: '#363636',
-    fontSize: '80px',
-  },
-  body: {
-    color: '#636363',
-    fontSize: '20px',
-  },
-};
+const Container = styled.div`
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontFamily: 'Helvetica',
+  margin: 'auto'
+`;
+
+const Box = styled.div`
+  backgroundColor: '#ededed',
+  width: '60%',
+  height: '60%',
+  padding: '30px 100px',
+  borderRadius: '10px',
+  boxShadow: '1px 1px 10px rgba(0,0,0,0.2)'
+`;
+
+const Header = styled.h1`
+  color: '#363636',
+  fontSize: '80px',
+`;
+
+const Body = styled.p`
+  color: '#636363',
+  fontSize: '20px',
+`;
 
 function NotFoundPage() {
+  // const store = createStore(reducers);
+
   const [hover, setHover] = useState(false);
 
   const mouseEnter = () => {
@@ -41,14 +47,14 @@ function NotFoundPage() {
   };
 
   return (
-    <div style={styles.container}>
-      <div style={styles.box}>
-        <h1 style={styles.header}>404!</h1>
-        <p style={styles.body}>
+    <Container>
+      <Box>
+        <Header>404!</Header>
+        <Body>
           Page Not Found - the page you&apos;re looking for doesn&apos;t exist
           or an error occurred :(
-        </p>
-        <p style={{ ...styles.body, color: '#57d9eb' }}>
+        </Body>
+        <Body style={{ color: '#57d9eb' }}>
           <Link
             onMouseEnter={() => mouseEnter()}
             onMouseLeave={() => mouseLeave()}
@@ -69,10 +75,11 @@ function NotFoundPage() {
           >
             &larr; Click here to go back.
           </Link>
-        </p>
-      </div>
-    </div>
+        </Body>
+      </Box>
+    </Container>
   );
 }
 
 export default NotFoundPage;
+
--- src/client/src/routing/UnauthenticatedRoute.tsx
+++ src/client/src/routing/UnauthenticatedRoute.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-function PrivateRoute({ children }: any) {
+function UnauthenticatedRoute({ children }: any) {
   const isAuthenticated = true; // dummy value right now
   return isAuthenticated ? <Navigate to="/" /> : children;
 }
 
-export default PrivateRoute;
+export default UnauthenticatedRoute;
