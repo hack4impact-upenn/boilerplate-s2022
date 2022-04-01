3db14bf6a6808922a5e7517b9c6086825ecf4d80
danielbarra24 <72272441+danielbarra24@users.noreply.github.com>
2022-04-01 17:28:39 -0400

    minor change to eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,11 +4,6 @@ module.exports = {
   rules: {
     'no-console': 'warn',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    // Add overriden rules specific to server here
-    // 'no-underscore-dangle': [
-    //   'error',
-    //   { allowAfterThis: true, allow: ['_instance'] },
-    // ],
   },
   env: {
     browser: true,
