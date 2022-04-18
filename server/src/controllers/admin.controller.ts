800fcacca776b1fc1e9092701be312f62f9ccaf7
Adi Bhati <aditya.bhati2001@gmail.com>
2022-04-17 22:13:57 -0400

    fixed logic and error codes in deleteuser

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -44,14 +44,14 @@ const deleteUser = async (req: express.Request, res: express.Response) => {
   // check if user to delete is an admin
   const user: IUser | null = await retrieveUser(email);
   if (!user) {
-    return res.status(401).send({
+    return res.status(400).send({
       message: `User to delete does not exist`,
     });
   }
-  if (!user.admin) {
-    return res.status(401).send({ message: 'Cannot delete an admin' });
+  if (user.admin) {
+    return res.status(400).send({ message: 'Cannot delete an admin' });
   }
-  // delete user
+  // Delete user
   return deleteOne(email)
     .then(() => res.sendStatus(201))
     .catch((e) => {
