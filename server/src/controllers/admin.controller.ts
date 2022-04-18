5242de5c2d89c4c1e7baac5a3abda4f627830217
Adi Bhati <aditya.bhati2001@gmail.com>
2022-04-17 22:10:30 -0400

    Changed function names to upgrade

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -1,37 +1,45 @@
 import express from 'express';
 import { IUser } from '../models/user';
-import { toggleAdmin, retrieveUser, retrieveAllUsers, deleteOne } from '../services/user.service';
+import {
+  upgradeToAdmin,
+  retrieveUser,
+  retrieveAllUsers,
+  deleteOne,
+} from '../services/user.service';
 
-const getAllUsers = async(req: express.Request, res: express.Response) => {
+const getAllUsers = async (req: express.Request, res: express.Response) => {
   // return all users
   return retrieveAllUsers()
     .then((userList) => {
-      res.send(userList)
+      res.send(userList);
     })
-    .catch((e) => {res.status(400).send({ message: e});
-  });
+    .catch((e) => {
+      res.status(400).send({ message: e });
+    });
 };
 
-const upgradePrivilege = async (req: express.Request, res: express.Response) => {
+const upgradePrivilege = async (
+  req: express.Request,
+  res: express.Response,
+) => {
   // Check if user exists
   const email = req.body.email;
-  console.log('switchPrivilege', email)
+  console.log('switchPrivilege', email);
   const user: IUser | null = await retrieveUser(email);
   if (!user) {
     return res.status(401).send({
       message: `User with email ${email} does not exist.`,
     });
   }
-  // Toggles User's Admin Status
-  return toggleAdmin(email)
+  // Upgrade's the user's admin status
+  return upgradeToAdmin(email)
     .then(() => res.sendStatus(200))
     .catch((e) => {
-      console.log(e);
       res.status(400).send({ message: e });
     });
 };
 
-const deleteUser = async(req: express.Request, res: express.Response) => {
+const deleteUser = async (req: express.Request, res: express.Response) => {
   const email = req.params.email;
   // check if user to delete is an admin
   const user: IUser | null = await retrieveUser(email);
@@ -43,11 +51,12 @@ const deleteUser = async(req: express.Request, res: express.Response) => {
   if (!user.admin) {
     return res.status(401).send({ message: 'Cannot delete an admin' });
   }
-  // delete user 
+  // delete user
   return deleteOne(email)
     .then(() => res.sendStatus(201))
-    .catch((e) => {res.status(400).send({ message: e });
-  });
+    .catch((e) => {
+      res.status(400).send({ message: e });
+    });
 };
 
-export { getAllUsers, upgradePrivilege as switchPrivilege, deleteUser };
+export { getAllUsers, upgradePrivilege, deleteUser };
--- server/src/routes/admin.route.ts
+++ server/src/routes/admin.route.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 import { isAdmin } from '../controllers/admin.middleware';
-import { getAllUsers, switchPrivilege, deleteUser } from '../controllers/admin.controller';
-import { ensureAuthenticated } from '../controllers/auth.middleware';
+import {
+  getAllUsers,
+  upgradePrivilege,
+  deleteUser,
+} from '../controllers/admin.controller';
+import ensureAuthenticated from '../controllers/auth.middleware';
 import 'dotenv/config';
 
 const router = express.Router();
 
-router.get('/allusers', ensureAuthenticated, isAdmin, getAllUsers)
-router.put('/upgrade-privilege', ensureAuthenticated, isAdmin, switchPrivilege)
-router.delete('user/:email', ensureAuthenticated, isAdmin, deleteUser)
+router.get('/allusers', ensureAuthenticated, isAdmin, getAllUsers);
+router.put(
+  '/upgrade-privilege',
+  ensureAuthenticated,
+  isAdmin,
+  upgradePrivilege,
+);
+router.delete('user/:email', ensureAuthenticated, isAdmin, deleteUser);
 
 export default router;
