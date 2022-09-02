c11093487495c5b65fa2e0dd50e361d7d869c097
Ben Demers <ben.n.demers@gmail.com>
2022-09-02 16:26:37 -0400

    theme and styled grid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-lone-blocks */
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -32,40 +34,42 @@ function App() {
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <ThemeProvider theme={theme}>
-              <Routes>
-                {/* Routes accessed only if user is not authenticated */}
-                <Route element={<UnauthenticatedRoutesWrapper />}>
-                  <Route path="/login" element={<LoginView />} />
-                  <Route path="/register" element={<RegisterPage />} />
+              <CssBaseline>
+                <Routes>
+                  {/* Routes accessed only if user is not authenticated */}
+                  <Route element={<UnauthenticatedRoutesWrapper />}>
+                    <Route path="/login" element={<LoginView />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route
+                      path="/verify-account/:token"
+                      element={<VerifyAccountPage />}
+                    />
+                    <Route
+                      path="/email-reset"
+                      element={<ResetPasswordEmailPage />}
+                    />
+                    <Route
+                      path="/reset-password/:token"
+                      element={<ResetPasswordPage />}
+                    />
+                  </Route>
+                  {/* Routes accessed only if user is authenticated */}
+                  <Route element={<ProtectedRoutesWrapper />}>
+                    <Route path="/home" element={<HomeView />} />
+                  </Route>
+                  <Route element={<AdminRoutesWrapper />}>
+                    <Route path="/users" element={<AdminDashboard />} />
+                  </Route>
+                  {/* Element depends on of if users is authorized or unauthorized */}
                   <Route
-                    path="/verify-account/:token"
-                    element={<VerifyAccountPage />}
+                    path="/"
+                    element={
+                      <DynamicRedirect unAuthPath="/login" authPath="/home" />
+                    }
                   />
-                  <Route
-                    path="/email-reset"
-                    element={<ResetPasswordEmailPage />}
-                  />
-                  <Route
-                    path="/reset-password/:token"
-                    element={<ResetPasswordPage />}
-                  />
-                </Route>
-                {/* Routes accessed only if user is authenticated */}
-                <Route element={<ProtectedRoutesWrapper />}>
-                  <Route path="/home" element={<HomeView />} />
-                </Route>
-                <Route element={<AdminRoutesWrapper />}>
-                  <Route path="/users" element={<AdminDashboard />} />
-                </Route>
-                {/* Element depends on of if users is authorized or unauthorized */}
-                <Route
-                  path="/"
-                  element={
-                    <DynamicRedirect unAuthPath="/login" authPath="/home" />
-                  }
-                />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
+                  <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+              </CssBaseline>
             </ThemeProvider>
           </PersistGate>
         </Provider>
--- client/src/LoginRegister/RegisterPage.tsx
+++ client/src/LoginRegister/RegisterPage.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Link, TextField, Button } from '@mui/material';
-import { ThemeProvider } from '@mui/system';
 import { useNavigate, Link as RouterLink } from 'react-router-dom';
-import theme from '../assets/theme';
 import { RegisterValidation } from './inputValidation';
 import ErrorMessage from './errorMessage';
 
@@ -40,100 +38,98 @@ function RegisterPage() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <ScreenGrid>
-        <FormGridCol>
-          <FormField>
-            <FormHeaderText>
-              We are so excited to have you on board!
-            </FormHeaderText>
-          </FormField>
-          <FormField>
-            <TextField
-              error={error === 'empty'}
-              helperText={<ErrorMessage error={error} />}
-              size="small"
-              type="text"
-              required
-              label="First Name"
-              value={first}
-              onChange={(e) => setFirst(e.target.value)}
-            />
-          </FormField>
-          <FormField>
-            <TextField
-              error={error === 'empty'}
-              helperText={<ErrorMessage error={error} />}
-              size="small"
-              type="text"
-              required
-              label="Last Name"
-              value={last}
-              onChange={(e) => setLast(e.target.value)}
-            />
-          </FormField>
-          <FormField>
-            <TextField
-              error={
-                error === 'empty' ||
-                error === 'badName' ||
-                error === 'badEmail' ||
-                error === 'duplicate'
-              }
-              helperText={<ErrorMessage error={error} />}
-              size="small"
-              type="email"
-              required
-              label="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormField>
-          <FormField>
-            <TextField
-              error={error === 'empty' || error === 'mismatch'}
-              helperText={<ErrorMessage error={error} />}
-              size="small"
-              type="password"
-              required
-              label="Password"
-              value={password1}
-              onChange={(e) => setPassword1(e.target.value)}
-            />
-          </FormField>
-          <FormField>
-            <TextField
-              error={error === 'empty' || error === 'mismatch'}
-              helperText={<ErrorMessage error={error} />}
-              size="small"
-              type="password"
-              required
-              label="Confirm Password"
-              value={password2}
-              onChange={(e) => setPassword2(e.target.value)}
-            />
-          </FormField>
-          <FormField>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              onClick={() => onSubmit()}
-            >
-              Signup
-            </Button>
-          </FormField>
-          <FormField>
-            <MiniLinkText>
-              Back to{' '}
-              <Link component={RouterLink} to="/login">
-                Login
-              </Link>
-            </MiniLinkText>
-          </FormField>
-        </FormGridCol>
-      </ScreenGrid>
-    </ThemeProvider>
+    <ScreenGrid>
+      <FormGridCol>
+        <FormField>
+          <FormHeaderText>
+            We are so excited to have you on board!
+          </FormHeaderText>
+        </FormField>
+        <FormField>
+          <TextField
+            error={error === 'empty'}
+            helperText={<ErrorMessage error={error} />}
+            size="small"
+            type="text"
+            required
+            label="First Name"
+            value={first}
+            onChange={(e) => setFirst(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <TextField
+            error={error === 'empty'}
+            helperText={<ErrorMessage error={error} />}
+            size="small"
+            type="text"
+            required
+            label="Last Name"
+            value={last}
+            onChange={(e) => setLast(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <TextField
+            error={
+              error === 'empty' ||
+              error === 'badName' ||
+              error === 'badEmail' ||
+              error === 'duplicate'
+            }
+            helperText={<ErrorMessage error={error} />}
+            size="small"
+            type="email"
+            required
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <TextField
+            error={error === 'empty' || error === 'mismatch'}
+            helperText={<ErrorMessage error={error} />}
+            size="small"
+            type="password"
+            required
+            label="Password"
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <TextField
+            error={error === 'empty' || error === 'mismatch'}
+            helperText={<ErrorMessage error={error} />}
+            size="small"
+            type="password"
+            required
+            label="Confirm Password"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={() => onSubmit()}
+          >
+            Signup
+          </Button>
+        </FormField>
+        <FormField>
+          <MiniLinkText>
+            Back to{' '}
+            <Link component={RouterLink} to="/login">
+              Login
+            </Link>
+          </MiniLinkText>
+        </FormField>
+      </FormGridCol>
+    </ScreenGrid>
   );
 }
 
