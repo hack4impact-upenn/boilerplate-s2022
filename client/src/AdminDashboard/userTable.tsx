0b9efb86f97486ebec2ee1139612e8533999b558
Ben Demers <ben.n.demers@gmail.com>
2022-09-26 17:01:15 -0400

    name change

--- client/src/AdminDashboard/userTable.tsx
+++ client/src/AdminDashboard/userTable.tsx
@@ -1,8 +1,6 @@
 /**
  * A file that contains all the components and logic for the table of users
- * in the AdminDaaashboardPage. The components are ordered in increasing
- * complexity, with the final component being the UserTable at the end of the
- * file.
+ * in the AdminDaaashboardPage.
  */
 import React, { useEffect, useState } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -26,10 +24,6 @@ interface AdminDashboardRow {
 /**
  * The standalone table component for holding information about the users in
  * the database and allowing admins to remove users and promote users to admins.
- *
- * This component is wrapper component for {@link UserTableBodyWrapper} and
- * handles logic for fetching the data for the user table and showing a loaading
- * icon while the data is loading.
  */
 function UserTable() {
   const columns: TColumn[] = [
