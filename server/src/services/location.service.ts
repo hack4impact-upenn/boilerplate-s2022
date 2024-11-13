da5c73040d31dce547f833d55c7c3b12e1786b08
Irenka Ni <ni.irenka@gmail.com>
2024-11-12 19:42:01 -0500

    hopefully will push to right branch

--- server/src/services/location.service.ts
+++ server/src/services/location.service.ts
@@ -1,27 +1,30 @@
-import { ILocation, Location } from "../models/location.model.ts";
+import { ILocation, Location } from '../models/location.model.ts';
 
 const createLocation = async (person: ILocation) => {
-    const newLocation = new Location(person);
-    const result = await newLocation.save();
-    return result;
+  const newLocation = new Location(person);
+  const result = await newLocation.save();
+  return result;
 };
 
 const getAllLocations = async () => {
-    const locationList = await Location.find({}).exec();
-    return locationList;
+  const locationList = await Location.find({}).exec();
+  return locationList;
 };
 
 const getLocationByName = async (name: string) => {
-    const location = await Location.findOne({ name })
-    .exec();
+  const location = await Location.findOne({ name }).exec();
   return location;
-}
+};
 
 const deleteLocationByName = async (name: string) => {
-    const location = await Location.findByIdAndDelete(name).exec();
-    return location;
-}
-  
-  // eslint-disable-next-line import/prefer-default-export
-  export { createLocation, getAllLocations, getLocationByName, deleteLocationByName };
+  const location = await Location.findByIdAndDelete(name).exec();
+  return location;
+};
 
+// eslint-disable-next-line import/prefer-default-export
+export {
+  createLocation,
+  getAllLocations,
+  getLocationByName,
+  deleteLocationByName,
+};
