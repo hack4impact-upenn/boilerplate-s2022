adf06e532e834cf8a527134e16e7e2c9aa3ef13d
Su Yeon Lee <suyeonlee@hnt-08096.apn.wlan.private.upenn.edu>
2025-01-26 15:31:02 -0500

    logo

--- client/src/sidebar/Sidebar.tsx
+++ client/src/sidebar/Sidebar.tsx
@@ -6,7 +6,13 @@ const Sidebar = () => {
   return (
     <div className="sidebar-container">
       {/* Logo Section */}
-
+      <div className="sidebar-logo">
+        <img 
+            src={require('../assets/logo.png')} 
+            alt="Children's Environmental Literacy Foundation Logo"
+            className="sidebar-logo-image"
+          />
+      </div>
       {/* Buttons Section */}
       <div className="sidebar-card">
         <button className="sidebar-button">
